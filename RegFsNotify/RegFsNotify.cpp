e748352e1816a41aa30465efa59fb6e14458243e
unknown <>
2016-07-10 20:46:30 +0900

    원본 RegFsNotify (주석없음,변경없음)

---
+++ RegFsNotify/RegFsNotify.cpp
@@ -0,0 +1,123 @@
+/*
+# Copyright (C) 2010 Michael Ligh
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "mon.h"
+
+ HANDLE g_hFile;
+ HANDLE g_hStopEvent;
+ HANDLE g_hRegWatch[2];
+
+USHORT GetConsoleTextAttribute(HANDLE hConsole)
+{
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(hConsole, &csbi);
+    return(csbi.wAttributes);
+}
+
+void Output(USHORT Color, LPTSTR format, ... )
+{
+	va_list args;
+	int len;
+	DWORD cb;
+	LPTSTR buffer;
+
+	va_start(args, format);
+	len = _vsctprintf(format, args) + sizeof(TCHAR); 
+	buffer = new TCHAR[len * sizeof(TCHAR)];
+
+	if (!buffer) { 
+		return;
+	}
+
+	_vstprintf_s(buffer, len, format, args);
+
+	if (g_hFile != INVALID_HANDLE_VALUE) {
+#ifdef _UNICODE
+		LPSTR str = new CHAR[len + 1];
+		if (str) { 
+			memset(str, 0, len + 1);
+			WideCharToMultiByte(CP_ACP, 0, 
+				buffer, -1, str, len, NULL, NULL);
+			WriteFile(g_hFile, str, strlen(str), &cb, NULL);
+			delete[] str;
+		}
+#else 
+	WriteFile(g_hFile, buffer, strlen(buffer), &cb, NULL);
+#endif
+	} 
+
+	HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	if (Color) 
+	{ 
+		SetConsoleTextAttribute(
+			Handle, Color | FOREGROUND_INTENSITY);
+	} 
+
+	_tprintf(buffer);
+
+	SetConsoleTextAttribute(Handle, 
+		FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED);
+
+   delete[] buffer;
+}
+ 
+BOOL CtrlHandler(DWORD fdwCtrlType) 
+{ 
+	switch(fdwCtrlType) 
+	{ 
+    case CTRL_C_EVENT: 
+		SetEvent(g_hStopEvent);
+		return TRUE;
+    case CTRL_CLOSE_EVENT: 
+		SetEvent(g_hStopEvent);
+		return TRUE; 
+    case CTRL_BREAK_EVENT: 
+		return FALSE; 
+    case CTRL_LOGOFF_EVENT: 
+		return FALSE; 
+    case CTRL_SHUTDOWN_EVENT: 
+		return FALSE; 
+    default: 
+		return FALSE; 
+	} 
+} 
+
+void _tmain(int argc, TCHAR *argv[])
+{
+	g_hStopEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+	SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE);
+
+	g_hFile = CreateFile(_T("RegFsNotify.txt"), 
+		GENERIC_WRITE, 
+		FILE_SHARE_READ, 0, 
+		CREATE_ALWAYS, 0, NULL);
+
+	HANDLE hThread[2];
+
+	hThread[0] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)StartFileMonitor, NULL, 0, NULL);
+	hThread[1] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)StartRegistryMonitor, NULL, 0, NULL);
+
+	WaitForMultipleObjects(2, (const HANDLE*)&hThread, TRUE, INFINITE);
+	TerminateThread(g_hRegWatch[0], 0);
+	TerminateThread(g_hRegWatch[1], 0);
+
+	CloseHandle(g_hStopEvent);
+	CloseHandle(g_hFile);
+
+	_tprintf(_T("Program terminating.\n"));
+}
---
+++ RegFsNotify/file.cpp
@@ -0,0 +1,181 @@
+/*
+# Copyright (C) 2010 Michael Ligh
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "mon.h"
+
+// maximum number of drives to monitor 
+#define MAX_DRIVES 24
+
+// global variables for change notifications
+HANDLE  g_ChangeHandles[MAX_DRIVES];
+HANDLE  g_DirHandles[MAX_DRIVES];
+LPTSTR  g_szDrives[MAX_DRIVES];
+DWORD   g_idx = 0;
+
+void ProcessChange(int idx)
+{
+	BYTE buf[32 * 1024];
+	DWORD cb = 0;
+	PFILE_NOTIFY_INFORMATION pNotify;
+	int offset = 0;
+	TCHAR szFile[MAX_PATH*2];
+
+	memset(buf, 0, sizeof(buf));
+
+	// find out what type of change triggered the notification
+	if (ReadDirectoryChangesW(g_DirHandles[idx], buf, 
+		sizeof(buf), TRUE, 
+		FILE_CHANGE_FLAGS, &cb, NULL, NULL))
+	{
+		// parse the array of file information structs
+		do {
+			pNotify = (PFILE_NOTIFY_INFORMATION) &buf[offset];
+			offset += pNotify->NextEntryOffset;
+
+			memset(szFile, 0, sizeof(szFile));
+
+			memcpy(szFile, pNotify->FileName, 
+				pNotify->FileNameLength);
+
+			// if the file is whitelisted, go to the next
+			if (IsWhitelisted(szFile)) {
+				continue;
+			}
+
+			switch (pNotify->Action)
+			{
+			case FILE_ACTION_ADDED:
+				Output(FOREGROUND_GREEN, 
+					_T("[ADDED] %s%s\n"), g_szDrives[idx], szFile);
+				break;
+			case FILE_ACTION_REMOVED: 
+				Output(FOREGROUND_RED, 
+					_T("[REMOVED] %s%s\n"), g_szDrives[idx], szFile);
+				break;
+			case FILE_ACTION_MODIFIED: 
+				Output(0, _T("[MODIFIED] %s%s\n"), 
+					g_szDrives[idx], szFile);
+				break;
+			case FILE_ACTION_RENAMED_OLD_NAME:
+				Output(0, _T("[RENAMED (OLD)] %s%s"), 
+					g_szDrives[idx], szFile);
+				break; 
+			case FILE_ACTION_RENAMED_NEW_NAME:
+				Output(0,_T("[RENAMED (NEW)] %s%s"), 
+					g_szDrives[idx], szFile);
+				break;
+			default:
+				Output(0,_T("[??] %s%s\n"), 
+					g_szDrives[idx], szFile);
+				break;
+			};
+		} while (pNotify->NextEntryOffset != 0);
+	}
+}
+
+void StartFileMonitor(void)
+{
+	DWORD dwWaitStatus;
+	BOOL  bOK = FALSE;
+	TCHAR   pszList[1024];
+	DWORD   ddType;
+	LPTSTR  pStart = NULL;
+	HANDLE  hChange, hDir;
+
+	// get a list of logical drives
+	memset(pszList, 0, sizeof(pszList));
+	GetLogicalDriveStrings(sizeof(pszList), pszList);
+
+	// parse the list of null-terminated drive strings
+	pStart = pszList;
+	while(_tcslen(pStart)) 
+	{
+		ddType = GetDriveType(pStart);
+
+		// only monitor local and removable (i.e. USB) drives
+		if ((ddType == DRIVE_FIXED || ddType == DRIVE_REMOVABLE) && 
+			_tcscmp(pStart, _T("A:\\")) != 0)
+		{
+			hChange = FindFirstChangeNotification(pStart,
+									TRUE, /* watch subtree */
+								    FILE_CHANGE_FLAGS);
+
+			if (hChange == INVALID_HANDLE_VALUE)
+				continue;
+
+			hDir = CreateFile(pStart, 
+				FILE_LIST_DIRECTORY, 
+				FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+				NULL, 
+				OPEN_EXISTING, 
+				FILE_FLAG_BACKUP_SEMANTICS, /* opens a directory */
+				NULL);
+
+			if (hDir == INVALID_HANDLE_VALUE) {
+				FindCloseChangeNotification(hChange);
+				continue;
+			}
+
+			_tprintf(_T("Monitoring %s\n"), pStart);
+
+			// save the handles and drive letter 
+			g_szDrives[g_idx]      = _tcsdup(pStart);
+			g_DirHandles[g_idx]    = hDir;
+			g_ChangeHandles[g_idx] = hChange;
+			g_idx++;
+		}
+
+		pStart += wcslen(pStart) + 1;
+	}
+ 
+	// wait for a notification to occur
+	while(WaitForSingleObject(g_hStopEvent, 1) != WAIT_OBJECT_0) 
+	{
+		dwWaitStatus = WaitForMultipleObjects(
+			g_idx, 
+			g_ChangeHandles, 
+			FALSE, INFINITE); 
+
+		bOK = FALSE;
+
+		// if the wait suceeded, for which handle did it succeed?
+		for(int i=0; i < g_idx; i++)
+		{
+			if (dwWaitStatus == WAIT_OBJECT_0 + i) 
+			{
+				bOK = TRUE;
+				ProcessChange(i);
+				if (!FindNextChangeNotification(g_ChangeHandles[i])) 
+				{
+					SetEvent(g_hStopEvent);
+					break;
+				}
+				break;
+			}
+		}
+
+		// the wait failed or timed out
+		if (!bOK) break;
+	}
+	_tprintf(_T("Got stop event...\n"));
+
+	for(int i=0; i < g_idx; i++)
+	{
+		_tprintf(_T("Closing handle for %s\n"), g_szDrives[i]);
+		CloseHandle(g_DirHandles[i]);
+		FindCloseChangeNotification(g_ChangeHandles[i]);
+	}
+}
