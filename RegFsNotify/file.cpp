53a2961a5be56f0f63b8db221d8e3142610b21c8
kgoung123 <kgoung123@naver.com>
2016-07-13 11:39:25 +0900

    추출해왔던 파일명에서 특정 확장자를 가진 파일명만 배열에 넣기 -> 확장자를 비교해 배열에 넣어야함

--- RegFsNotify/file.cpp
+++ RegFsNotify/file.cpp
@@ -1,5 +1,6 @@
 #include "mon.h"
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_DRIVES 24
 #define MAX_VALUE_NAME 16383
@@ -10,14 +11,22 @@ LPTSTR  g_szDrives[MAX_DRIVES];
 DWORD   g_idx = 0;
 
 //변경한 부분
-TCHAR * roaming_file_name=_T(""); //로밍부분 파싱해서 얻은 파일 명
-TCHAR * prefetch_file_name=_T(""); //프리패치에서 파싱해서 얻은 파일 명
+TCHAR * Rname; //확장자 판단하기 위해 원본 파일명을 복사해 담아두는 변수
+TCHAR * Pname; //
+
+TCHAR * roaming_file_name[MAX_VALUE_NAME]; //로밍부분 파싱해서 얻은 파일 명
+TCHAR * prefetch_file_name[MAX_VALUE_NAME]; //프리패치에서 파싱해서 얻은 파일 명
 
 TCHAR s1[MAX_VALUE_NAME]=_T("Roaming");
 TCHAR s2[MAX_VALUE_NAME]=_T("Prefetch");
 
 DWORD Ridx=0; //로밍부분 인덱스 초기화
 DWORD Pidx=0; //프리패치 인덱스 초기화
+
+//확장자 판단
+TCHAR *token;
+const TCHAR str1[] = _T(".");
+const TCHAR str2[] = _T("txt");
 
 void parsing(DWORD flag, TCHAR * filename);
 
@@ -31,6 +40,13 @@ void ProcessChange(int idx)
 	int offset = 0;
 	TCHAR szFile[MAX_PATH*2];
 	memset(buf, 0, sizeof(buf));
+
+	//NULL로 초기화
+	memset(roaming_file_name, '\0', sizeof(roaming_file_name));
+	memset(prefetch_file_name, '\0', sizeof(prefetch_file_name));
+
+	//_tprintf(_T("%c\n"), str );
+
 
 	if (ReadDirectoryChangesW(g_DirHandles[idx], buf, 
 		sizeof(buf), TRUE, 
@@ -47,8 +63,8 @@ void ProcessChange(int idx)
 			memcpy(szFile, pNotify->FileName, 
 				pNotify->FileNameLength);
 
-			_tprintf(_T("--------- %s --------- \n"), szFile);
-			_tprintf(_T("---------------------------------------------------- \n"));
+			//_tprintf(_T("--------- %s --------- \n"), szFile);
+			//_tprintf(_T("---------------------------------------------------- \n"));
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -56,12 +72,34 @@ void ProcessChange(int idx)
 			// ---------------------- Roaming ---------------------- 
 			if (RoamingWhitelisted(szFile)) { 
 				
-				roaming_file_name = _tcsstr(szFile, s1);
-				Output_Roaming(FOREGROUND_BLUE, _T("[ROAMING] ------> %s \n"), roaming_file_name+8);
+				roaming_file_name[Ridx] = _tcsstr(szFile, s1)+8;
+
+				//원래 파일명 보존을 위해 변수에 복사
+				Rname = _tcsstr(roaming_file_name[Ridx], str1); //(abc.txt, .)
+				_tprintf(_T("+++++++++++++++++++ %s ++++++++++++++++++++ \n"), Rname);
+
+				_tprintf(_T("[File Name] --------- %s --------- \n"), roaming_file_name[Ridx]);
+
+				////////////////////////////////////////////////////////////////////////////////////////////////////////
 				
+				//확장자가 txt가 맞다면 배열에 넣었었던 파일명을 출력 
+				//if(_tcsicmp(par[1], str2) == 0)
+				//{
+				//	Output_Roaming(FOREGROUND_RED, _T("ROAMING [%d] ------> %s \n"), Ridx, roaming_file_name[Ridx]);
+				//	Ridx++;
+				//	_tprintf(_T("[SUCCESS]---------------------------------------------------------------------------- \n"));
+				//}
+				////아닐경우 배열에 넣었었던 값을 지움
+				//else
+				//{
+				//	roaming_file_name[Ridx] = '\0';
+				//	_tprintf(_T("[NOT]---------------------------------------------------------------------------- \n"));
+				//}
+
 				//버퍼 생성
 
 				//ExtractProcess(resultBuffer);
+
 
 				//*********ADDED*********
 				switch (pNotify->Action)
@@ -69,18 +107,18 @@ void ProcessChange(int idx)
 				case FILE_ACTION_ADDED:
 					Output_Roaming(FOREGROUND_GREEN, _T("[ADDED] %s%s \n"), g_szDrives[idx], szFile);
 					break;
-				case FILE_ACTION_REMOVED: 
+				/*case FILE_ACTION_REMOVED: 
 					Output_Roaming(FOREGROUND_RED, _T("\n [REMOVED] %s%s \n"), g_szDrives[idx], szFile);
-					break;
+					break;*/
 				case FILE_ACTION_MODIFIED: 
 					Output_Roaming(0, _T("[MODIFIED] %s%s \n"),g_szDrives[idx], szFile);
 					break;
-				case FILE_ACTION_RENAMED_OLD_NAME:
+				/*case FILE_ACTION_RENAMED_OLD_NAME:
 					Output_Roaming(0, _T("\n[RENAMED (OLD)] %s%s \n"), g_szDrives[idx], szFile);
 					break; 
 				case FILE_ACTION_RENAMED_NEW_NAME:
 					Output_Roaming(0, _T("\n[RENAMED (NEW)] %s%s \n"), g_szDrives[idx], szFile);
-					break;
+					break;*/
 				default:
 					Output_Roaming(0,_T("[??] %s%s \n"), 
 						g_szDrives[idx], szFile);
@@ -92,8 +130,11 @@ void ProcessChange(int idx)
 
 				// ---------------------- Prefetch ---------------------- 
 
-				prefetch_file_name = _tcsstr(szFile, s2);
-				Output_Prefetch(FOREGROUND_BLUE, _T("[PRFETCH] ------> %s \n"), prefetch_file_name+9);
+				prefetch_file_name[Pidx] = _tcsstr(szFile, s2)+9;
+				Output_Prefetch(FOREGROUND_BLUE, _T("PRFETCH [%d] ------> %s \n"), Pidx, prefetch_file_name[Pidx]);
+				_tprintf(_T("---------------------------------------------------------------------------- \n"));
+
+				Pidx++;
 
 				//버퍼 생성
 
@@ -105,18 +146,18 @@ void ProcessChange(int idx)
 				case FILE_ACTION_ADDED:
 					Output_Prefetch(FOREGROUND_GREEN, _T("\n[ADDED] %s%s\n"), g_szDrives[idx], szFile);
 					break;
-				case FILE_ACTION_REMOVED: 
+				/*case FILE_ACTION_REMOVED: 
 					Output_Prefetch(FOREGROUND_RED, _T("\n[REMOVED] %s%s\n"), g_szDrives[idx], szFile);
-					break;
+					break;*/
 				case FILE_ACTION_MODIFIED: 
 					Output_Prefetch(0, _T("[MODIFIED] %s%s \n"), g_szDrives[idx], szFile);
 					break;
-				case FILE_ACTION_RENAMED_OLD_NAME:
+				/*case FILE_ACTION_RENAMED_OLD_NAME:
 					Output_Prefetch(0, _T("[RENAMED (OLD)] %s%s \n"), g_szDrives[idx], szFile);
 					break; 
 				case FILE_ACTION_RENAMED_NEW_NAME:
 					Output_Prefetch(0,_T("\n[RENAMED (NEW)] %s%s \n"), g_szDrives[idx], szFile);
-					break;
+					break;*/
 				default:
 					Output_Prefetch(0,_T("[??] %s%s \n"), g_szDrives[idx], szFile);
 					break;
